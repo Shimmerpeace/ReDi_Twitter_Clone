3d0ccf08bf1a136bea461fb4c9d3da9a8f087ffc
Shimmerpeace <peacedamian1@gmail.com>
2025-06-16 15:18:09 +0200

    commit

--- twitter_clone/src/components/Sidebar.js
+++ twitter_clone/src/components/Sidebar.js
@@ -2,26 +2,41 @@
 
 import Link from "next/link";
 
-
-
 export default function Sidebar() {
   return (
     <div>
-       <Link href="/" className="logo text-[28px]" >🐦</Link>
-      <Link href="/" className="link" >Home</Link>
-      <Link href="#" className="link" >Explore</Link>
-      <Link href="#" className="link" >Notifications</Link>
-      <Link href="#" className="link" >Messages</Link>
-      <Link href="#" className="link" >Grok</Link>
-      <Link href="#" className="link" >Communities</Link>
-      <Link href="#" className="link" >Profile</Link>
-      <Link href="#" className="link" >More</Link>
+      <Link href="/" className="logo text-[28px]">
+        🐦
+      </Link>
+      <Link href="/" className="link">
+        Home
+      </Link>
+      <Link href="/explore" className="link">
+        Explore
+      </Link>
+      <Link href="/notifications" className="link">
+        Notifications
+      </Link>
+      <Link href="messages" className="link">
+        Messages
+      </Link>
+      <Link href="/user/profile" className="link">
+        Communities
+      </Link>
+      <Link href="#" className="link">
+        Profile
+      </Link>
+      <Link href="#" className="link">
+        More
+      </Link>
 
+      <Link href="/(modal)/user/profile" className="link">
+        Open My Profile Modal
+      </Link>
 
-      <Link href="/(modal)/user/profile" className="link" >Open My Profile Modal</Link>
-
-<Link href="/(modal)/user/profile/1234567890" className="link" >Open User 1234567890 Profile Modal</Link>
-     
+      <Link href="/(modal)/user/profile/1234567890" className="link">
+        Open User 1234567890 Profile Modal
+      </Link>
     </div>
   );
 }
